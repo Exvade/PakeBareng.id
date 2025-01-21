7c0ede4e14cad7e0d39a9025149ae13f4ffcf7dc
Rangga Mukti <r4n994mukti@gmail.com>
2025-01-21 18:49:57 +0700

    UPDATE responsive testimoni

--- src/components/Testimoni.jsx
+++ src/components/Testimoni.jsx
@@ -2,13 +2,13 @@ import { motion } from 'framer-motion';
 import testimonials from '../data/testimoni';
 
 const TestimonialCard = ({ quote, author, role, pic }) => (
-  <div className="flex-shrink-0 w-[300px] md:w-[500px] p-6 bg-white border rounded-lg">
-    <blockquote className="mb-6 text-lg text-black">"{quote}"</blockquote>
-    <div className="flex items-center gap-3">
-      <img src={pic} alt={author} className="object-cover w-10 h-10 rounded-full" />
+  <div className="flex flex-col flex-shrink-0 w-[300px] md:w-[500px] my-4 md:my-8 p-4 md:p-6 bg-white shadow-[5px_5px_0px_rgba(232,252,248,0.5)] rounded-xl md:rounded-2xl">
+    <blockquote className="mb-3 md:mb-6 text-sm md:text-lg text-black">"{quote}"</blockquote>
+    <div className="flex mt-auto items-center gap-3">
+      <img src={pic} alt={author} className="object-cover w-7 md:w-10 h-7 md:h-10 rounded-full" />
       <div>
-        <div className="font-semibold text-black">{author}</div>
-        <div className="text-sm text-slate-400">{role}</div>
+        <div className="font-semibold text-sm md:text-base text-black">{author}</div>
+        <div className="text-xs md:text-sm text-slate-400">{role}</div>
       </div>
     </div>
   </div>
@@ -22,7 +22,9 @@ const Testimoni = () => {
       <div className="mx-auto">
         <div className="px-4">
           <h2 className="mb-4 text-2xl font-bold text-center text-white md:text-3xl">Yang mereka katakan tentang pakebareng.id?</h2>
-          <p className="max-w-2xl mx-auto mb-12 text-sm text-center text-gray-400">Apa kata mereka tentang Pakebareng? Lihat pengalaman pengguna yang sudah merasakan kemudahan berbagi biaya langganan di sini! 🚀</p>
+          <p className="max-w-[330px] md:max-w-2xl mx-auto mb-5 md:mb-12 text-base md:text-lg text-center text-gray-400">
+            Apa kata mereka tentang Pakebareng? Lihat pengalaman pengguna yang sudah merasakan kemudahan berbagi biaya langganan di sini! 🚀
+          </p>
         </div>
 
         {/* Wrapper untuk animasi */}
